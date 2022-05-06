d6ae1981958c861db085a46f13aaab9939746cfc
Aykut Ulış <aykutulis@hotmail.com>
2022-05-06 07:18:01 +0300

    env config

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,32 @@ import { UserModule } from './user/user.module';
 import { ReportModule } from './report/report.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { APP_PIPE } from '@nestjs/core';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const cookieSession = require('cookie-session');
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), UserModule, ReportModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        database: configService.get<string>('DB_NAME'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USER'),
+        password: configService.get<string>('DB_PASSWORD'),
+        synchronize: true,
+        entities: ['dist/**/*.entity{.ts,.js}'],
+      }),
+    }),
+    UserModule,
+    ReportModule,
+  ],
   controllers: [AppController],
   providers: [
     AppService,
