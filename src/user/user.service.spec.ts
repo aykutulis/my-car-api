def8b56d6d061af1fd34dcead1b2718e26854c82
Aykut Ulış <aykutulis@hotmail.com>
2022-05-06 06:44:23 +0300

    user service empty test

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,12 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { Repository } from 'typeorm';
+
+import { User } from './user.entity';
 import { UserService } from './user.service';
 
 describe('UserService', () => {
   let service: UserService;
+  let fakeUserRepo: Partial<Repository<User>>;
 
   beforeEach(async () => {
+    fakeUserRepo = {};
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        UserService,
+        {
+          provide: 'UserRepository',
+          useValue: fakeUserRepo,
+        },
+      ],
     }).compile();
 
     service = module.get<UserService>(UserService);
