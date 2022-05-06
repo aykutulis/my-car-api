617a31b37464d86df098a9286e9b850094001b04
Aykut Ulış <aykutulis@hotmail.com>
2022-05-06 05:49:11 +0300

    signup testing

--- src/user/auth.service.spec.ts
+++ src/user/auth.service.spec.ts
@@ -1,12 +1,29 @@
+import { BadRequestException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+
 import { AuthService } from './auth.service';
+import { User } from './user.entity';
+import { UserService } from './user.service';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let fakeUserService: Partial<UserService>;
 
   beforeEach(async () => {
+    fakeUserService = {
+      findAllByEmail: () => Promise.resolve([] as User[]),
+      create: (email: string, password: string) =>
+        Promise.resolve({ id: 1, email, password } as User),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: UserService,
+          useValue: fakeUserService,
+        },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
@@ -14,5 +31,23 @@ describe('AuthService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('create a new user with a salted and hashed password', async () => {
+    const user = await service.signup('asd@asd.com', '123456');
+
+    expect(user.password).not.toEqual('123456');
+    const [salt, hash] = user.password.split('.');
+    expect(salt).toBeDefined();
+    expect(hash).toBeDefined();
+  });
+
+  it('throws error if user already exists', async () => {
+    fakeUserService.findAllByEmail = () =>
+      Promise.resolve([{ id: 1, email: 'asd@asd.com', password: '123456' }]);
+
+    await expect(service.signup('asd@asd.com', '123456')).rejects.toThrow(
+      BadRequestException,
+    );
   });
 });
