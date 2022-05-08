8e9b32525c8d1b2b3f293c856aa4729a8a960d96
Aykut Ulış <aykutulis@hotmail.com>
2022-05-08 21:06:01 +0300

    estimate - WIP

--- src/report/report.controller.ts
+++ src/report/report.controller.ts
@@ -1,9 +1,11 @@
 import {
   Controller,
+  Get,
   Post,
   Patch,
   Body,
   Param,
+  Query,
   UseGuards,
 } from '@nestjs/common';
 
@@ -12,12 +14,13 @@ import { Serialize } from '../interceptors/serialize-interceptor';
 import { AuthGuard } from '../guards/auth.guard';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { ReportDto } from './dtos/report.dto';
+import { ApproveReportDto } from './dtos/approve-report.dts';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
 import { CurrentUser } from '../user/decorators/current-user.decorator';
 import { User } from 'src/user/user.entity';
-import { ApproveReportDto } from './dtos/approve-report.dts';
 import { AdminGuard } from 'src/guards/admin.guard';
 
-@Controller('/report')
+@Controller('/reports')
 export class ReportController {
   constructor(private readonly reportService: ReportService) {}
 
@@ -32,5 +35,10 @@ export class ReportController {
   @UseGuards(AdminGuard)
   updateReport(@Param('id') id: number, @Body() body: ApproveReportDto) {
     return this.reportService.changeApproval(id, body.approved);
+  }
+
+  @Get()
+  getEstimate(@Query() query: GetEstimateDto) {
+    return query;
   }
 }
