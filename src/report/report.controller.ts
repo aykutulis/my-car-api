1bb3af66439d5b79203d70074befdfd1caeb6774
Aykut Ulış <aykutulis@hotmail.com>
2022-05-08 16:55:18 +0300

    create report endpoint

--- src/report/report.controller.ts
+++ src/report/report.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards } from '@nestjs/common';
 
-@Controller('report')
-export class ReportController {}
+import { ReportService } from './report.service';
+import { AuthGuard } from '../guards/auth-guard';
+import { CreateReportDto } from './dtos/create-report.dto';
+
+@Controller('/report')
+export class ReportController {
+  constructor(private readonly reportService: ReportService) {}
+
+  @Post()
+  @UseGuards(AuthGuard)
+  createReport(@Body() createReportDto: CreateReportDto) {
+    return this.reportService.create(createReportDto);
+  }
+}
--- src/report/report.service.ts
+++ src/report/report.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateReportDto } from './dtos/create-report.dto';
+
+import { Report } from './report.entity';
 
 @Injectable()
-export class ReportService {}
+export class ReportService {
+  constructor(
+    @InjectRepository(Report)
+    private readonly repo: Repository<Report>,
+  ) {}
+
+  create(reportDto: CreateReportDto) {
+    const report = this.repo.create(reportDto);
+    return this.repo.save(report);
+  }
+}
